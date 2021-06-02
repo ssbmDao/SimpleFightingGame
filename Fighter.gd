2bec4e13fa405b05afc75bb689f755f72bec7af5
GodofGrunts <dergottdergrunten@gmail.com>
2021-06-02 12:36:49 -0400

    Added hurt/hitboxes. Facing now determine in new Main_Game.gd.

--- Fighter.gd
+++ Fighter.gd
@@ -5,11 +5,10 @@ export var DASH_SPEED = 500
 export var player_number : int
 var movement = Vector2.ZERO #2D Vector
 var control_inputs = {}
-var facing = "right"
+export var facing_right = true
 onready var anim = $AnimationPlayer
 onready var sprite = $Sprite
-onready var left_ray = $LeftRayCast
-onready var right_ray = $RightRayCast
+onready var this_name = self.name
 
 enum STATE {ATTACK, GUARD, FREE}
 var state
@@ -20,7 +19,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_direction()
 	player_input()
 	animates_player()
 	movement = move_and_slide(movement)
@@ -29,7 +27,7 @@ func _process(delta):
 func animates_player():
 	if state == STATE.FREE:
 		if movement != Vector2.ZERO:
-			if (facing == "left" && control_inputs.left) || (facing == "right" && control_inputs.right):
+			if (!facing_right && control_inputs.left) || (facing_right && control_inputs.right):
 				anim.play("walk")
 			else:
 				anim.play_backwards("walk")
@@ -56,13 +54,9 @@ func animates_player():
 			
 
 # Allows us to generalize characters and not worry about sprite facing.
-func look_direction():
-	if left_ray.is_colliding() && facing != "left":
-		sprite.flip_h = true
-		facing = "left"
-	if right_ray.is_colliding() && facing != "right":
-		sprite.flip_h = true
-		facing = "right"
+func change_direction():
+	scale.x = -1
+	facing_right = !facing_right
 
 
 # To share the same code for each player we have this function to determine
@@ -89,7 +83,14 @@ func player_input():
 			}
 		_:
 			print("No player number entered. You sure you're in the right scene?")
-			return
+			control_inputs = {
+				"left": Input.is_action_pressed("p1_left"),
+				"right": Input.is_action_pressed("p1_right"),
+				"punch": Input.is_action_just_pressed("p1_punch"),
+				"kick": Input.is_action_just_pressed("p1_kick"),
+				"block": Input.is_action_pressed("p1_block"),
+				"block_release": Input.is_action_just_released("p1_block")
+			}
 	player_movement()
 
 func player_movement():
@@ -104,3 +105,25 @@ func player_movement():
 			movement.x = 0
 	else:
 		movement.x = 0
+
+
+func _on_PunchHitBox_area_entered(area: Area2D) -> void:
+	if area.get_parent().name == this_name:
+		return
+	if area.name == "BlockBox":
+		print("Hit Block")
+	if area.name == "HurtBox":
+		print("Hit Character")
+	if area.name == "PunchHitBox":
+		print("clank")
+
+
+func _on_KickHitBox_area_entered(area: Area2D) -> void:
+	if area.get_parent().name == this_name:
+		return
+	if area.name == "BlockHitBox":
+		print("Hit Block")
+	if area.name == "HurtBox":
+		print("Hit Character")
+	if area.name == "KickHitBox":
+		print("clank")
