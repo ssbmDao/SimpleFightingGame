dd548e6645d33cb7c8aa2a446a975b85226b6324
GodofGrunts <dergottdergrunten@gmail.com>
2021-06-04 14:22:55 -0400

    Added dashing, but code is crazy. Might just use as a template.

--- Fighter.gd
+++ Fighter.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
 export var WALK_SPEED = 100
-export var DASH_SPEED = 500
+export var DASH_SPEED = 35
 export var player_number : int
+export var dash_cooldown_time = 1
 var movement = Vector2.ZERO #2D Vector
 var control_inputs = {}
 export var facing_right = true
@@ -10,18 +11,22 @@ onready var anim = $AnimationPlayer
 onready var sprite = $Sprite
 onready var this_name = self.name
 
-enum STATE {ATTACK, GUARD, FREE}
+enum STATE {ATTACK, GUARD, FREE, DASH}
 var state
+var dash_right = 0
+var dash_left = 0
+var dash_direction
+var dash_on_cooldown = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	state = STATE.FREE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# warning-ignore:unused_argument
 func _process(delta):
 	player_input()
 	animates_player()
-	movement = move_and_slide(movement)
 	
 
 func animates_player():
@@ -53,53 +58,87 @@ func animates_player():
 			state = STATE.FREE
 
 
-# To share the same code for each player we have this function to determine
-# to determine which inputs need to be read
-func player_input():
-	match player_number:
-		1:
-			control_inputs = {
-				"left": Input.is_action_pressed("p1_left"),
-				"right": Input.is_action_pressed("p1_right"),
-				"punch": Input.is_action_just_pressed("p1_punch"),
-				"kick": Input.is_action_just_pressed("p1_kick"),
-				"block": Input.is_action_pressed("p1_block"),
-				"block_release": Input.is_action_just_released("p1_block")
-			}
-		2:
-			control_inputs = {
-				"left": Input.is_action_pressed("p2_left"),
-				"right": Input.is_action_pressed("p2_right"),
-				"punch": Input.is_action_just_pressed("p2_punch"),
-				"kick": Input.is_action_just_pressed("p2_kick"),
-				"block": Input.is_action_pressed("p2_block"),
-				"block_release": Input.is_action_just_released("p2_block")
-			}
-		_:
-			print("No player number entered. You sure you're in the right scene?")
-			control_inputs = {
-				"left": Input.is_action_pressed("p1_left"),
-				"right": Input.is_action_pressed("p1_right"),
-				"punch": Input.is_action_just_pressed("p1_punch"),
-				"kick": Input.is_action_just_pressed("p1_kick"),
-				"block": Input.is_action_pressed("p1_block"),
-				"block_release": Input.is_action_just_released("p1_block")
-			}
-	player_movement()
-
 func player_movement():
 	if state == STATE.FREE:
 		if control_inputs.right && control_inputs.left:
 			movement.x = 0
-		elif control_inputs.right:
+		elif control_inputs.right || control_inputs.right_step:
+			if control_inputs.right_step:
+				for child in get_children():
+					if child.name == "dash_left_timer":
+						print("found left timer")
+						_on_dash_timer_timeout("left", child)
+				match dash_right:
+					0:
+						dash_right = 1
+						dash_direction = "right"
+						var dash_right_timer = Timer.new()
+						dash_right_timer.set_wait_time(.2)
+						dash_right_timer.connect("timeout", self, "_on_dash_timer_timeout", [dash_direction, dash_right_timer])
+						add_child(dash_right_timer)
+						dash_right_timer.start()
+						dash_right_timer.name = "dash_right_timer"
+					1:
+						if !dash_on_cooldown:
+							state = STATE.DASH
+						dash_right = 0
+					_:
+						return
 			movement.x = WALK_SPEED
-		elif control_inputs.left:
+		elif control_inputs.left || control_inputs.left_step:
+			if control_inputs.left_step:
+				for child in get_children():
+					if child.name == "dash_right_timer":
+						print("found right timer")
+						_on_dash_timer_timeout("right", child)
+				match dash_left:
+					0:
+						dash_left = 1
+						dash_direction = "left"
+						var dash_left_timer = Timer.new()
+						dash_left_timer.set_wait_time(.2)
+						dash_left_timer.connect("timeout", self, "_on_dash_timer_timeout", [dash_direction, dash_left_timer])
+						add_child(dash_left_timer)
+						dash_left_timer.start()
+						dash_left_timer.name = "dash_left_timer"
+					1:
+						if !dash_on_cooldown:
+							state = STATE.DASH
+						dash_left = 0
+					_:
+						return
 			movement.x = -WALK_SPEED
 		else:
 			movement.x = 0
 	else:
 		movement.x = 0
-
+	
+	if state == STATE.DASH:
+		dash_on_cooldown = true
+		var dash_cooldown_timer = Timer.new()
+		dash_cooldown_timer.connect("timeout", self, "_on_dash_cooldown_timer_timeout", [dash_cooldown_timer])
+		dash_cooldown_timer.name = "DashCooldownTimer"
+		dash_cooldown_timer.set_wait_time(dash_cooldown_time)
+		add_child(dash_cooldown_timer)
+		dash_cooldown_timer.start()
+		var d
+		match dash_direction:
+			"right":
+				d = DASH_SPEED
+				
+			"left":
+				d = -DASH_SPEED
+		var tween = Tween.new()
+		var cur_pos = position
+		tween.interpolate_property(self, "position", cur_pos, Vector2(cur_pos.x + d, position.y), .05, Tween.TRANS_LINEAR,Tween.EASE_OUT)
+		add_child(tween)
+		tween.start()
+		yield(tween, "tween_completed")
+		state = STATE.FREE
+	
+	if state != STATE.ATTACK:
+		movement = move_and_slide(movement)
+		#state = STATE.FREE
 
 func _on_PunchHitBox_area_entered(area: Area2D) -> void:
 	if area.get_parent().name == this_name:
@@ -116,8 +155,60 @@ func _on_KickHitBox_area_entered(area: Area2D) -> void:
 	if area.get_parent().name == this_name:
 		return
 	if area.name == "BlockHitBox":
-		print("Hit Block")
+		print("Hit Low on Block")
 	if area.name == "HurtBox":
 		print("Hit Character")
 	if area.name == "KickHitBox":
 		print("clank")
+
+func _on_dash_timer_timeout(direction, timer_node):
+	if state != STATE.ATTACK:
+		timer_node.queue_free()
+		if direction == "right":
+			dash_right = 0
+		elif direction == "left":
+			dash_left = 0
+
+func _on_dash_cooldown_timer_timeout(timer_node):
+	timer_node.queue_free()
+	dash_on_cooldown = false
+
+# To share the same code for each player we have this function to determine
+# to determine which inputs need to be read
+func player_input():
+	match player_number:
+		1:
+			control_inputs = {
+				"left": Input.is_action_pressed("p1_left"),
+				"left_step": Input.is_action_just_pressed("p1_left"),
+				"right": Input.is_action_pressed("p1_right"),
+				"right_step": Input.is_action_just_pressed("p1_right"),
+				"punch": Input.is_action_just_pressed("p1_punch"),
+				"kick": Input.is_action_just_pressed("p1_kick"),
+				"block": Input.is_action_pressed("p1_block"),
+				"block_release": Input.is_action_just_released("p1_block")
+			}
+		2:
+			control_inputs = {
+				"left": Input.is_action_pressed("p2_left"),
+				"left_step": Input.is_action_just_pressed("p2_left"),
+				"right": Input.is_action_pressed("p2_right"),
+				"right_step": Input.is_action_just_pressed("p2_right"),
+				"punch": Input.is_action_just_pressed("p2_punch"),
+				"kick": Input.is_action_just_pressed("p2_kick"),
+				"block": Input.is_action_pressed("p2_block"),
+				"block_release": Input.is_action_just_released("p2_block")
+			}
+		_:
+			print("No player number entered. You sure you're in the right scene?")
+			control_inputs = {
+				"left_step": Input.is_action_just_pressed("p1_left"),
+				"right": Input.is_action_pressed("p1_right"),
+				"right_step": Input.is_action_just_pressed("p1_right"),
+				"punch": Input.is_action_just_pressed("p1_punch"),
+				"kick": Input.is_action_just_pressed("p1_kick"),
+				"block": Input.is_action_pressed("p1_block"),
+				"block_release": Input.is_action_just_released("p1_block")
+			}
+	player_movement()
+
--- Main_Game.gd
+++ Main_Game.gd
@@ -1,15 +1,57 @@
 extends Node2D
 
+export var debug = true
+
 var characters
-var p1_start_pos = Vector2(300,480)
-var p2_start_pos = Vector2(700,480)
+var p1
+var p2
+var p1_start_pos = Vector2(136,230)
+var p2_start_pos = Vector2(376,230)
+var p1_current_side
+var p2_current_side
+
+onready var p2_label = $Player2State
 
 func _ready() -> void:
 	characters = get_tree().get_nodes_in_group("Characters")
 	for c in characters:
 		if c.player_number == 1:
 			c.global_position = p1_start_pos
+			p1_current_side = "left"
+			p1 = c
 		if c.player_number == 2:
 			c.global_position = p2_start_pos
 			c.scale.x = -1
 			c.facing_right = false
+			p2_current_side = "right"
+			p2 = c
+
+# warning-ignore:unused_argument
+func _process(delta: float) -> void:
+	check_positions()
+	if debug:
+		update_debug_labels()
+
+
+func check_positions():
+	if p1.position.x < p2.position.x && p1_current_side != "left":
+		print("CHANGE PLACES LEFT")
+		p1.scale.x = -1
+		p1.facing_right = true
+		p1_current_side = "left"
+		p2.scale.x = -1
+		p2.facing_right = false
+		p2_current_side = "right"
+	if p1.position.x > p2.position.x && p1_current_side != "right":
+		print("CHANGE PLACES RIGHT")
+		p1.scale.x = -1
+		p1.facing_right = false
+		p1_current_side = "right"
+		p2.scale.x = -1
+		p2.facing_right = true
+		p2_current_side = "left"
+		
+	
+func update_debug_labels():
+	p2_label.set_global_position(Vector2(p2.position.x, p2.position.y - 100))
+	p2_label.text = str(p2.state)
